c705234c4f31d091f9e634c9f37ab74b81a92401
Brayan Corvacho <alexiscorvachocd@gmail.com>
2026-06-04 23:35:35 -0500

    Initial backend project

---
+++ src/main/java/com/primefuel/fulltank/platform/FullTankPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.primefuel.fulltank.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullTankPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullTankPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/primefuel/fulltank/platform/FullTankPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primefuel.fulltank.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullTankPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
