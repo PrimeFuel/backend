20fb64d13c6954f880a535dab3e52b5ba103d9be
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 14:45:00 -0500

    feat(notification): add JPA repository for notification persistence

--- src/main/java/com/primefuel/fulltank/platform/notification/infrastructure/persistence/jpa/repositories/NotificationPersistenceRepository.java
+++ src/main/java/com/primefuel/fulltank/platform/notification/infrastructure/persistence/jpa/repositories/NotificationPersistenceRepository.java
@@ -1,4 +1,13 @@
 package com.primefuel.fulltank.platform.notification.infrastructure.persistence.jpa.repositories;
 
-public interface NotificationPersistenceRepository {
+import com.primefuel.fulltank.platform.notification.infrastructure.persistence.jpa.entities.NotificationPersistenceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationPersistenceRepository extends JpaRepository<NotificationPersistenceEntity, Long> {
+    List<NotificationPersistenceEntity> findByUserId(Long userId);
+    List<NotificationPersistenceEntity> findByUserIdAndReadFalse(Long userId);
 }
