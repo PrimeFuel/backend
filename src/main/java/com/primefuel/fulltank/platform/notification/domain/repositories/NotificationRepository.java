e7f720be439180e85b60667587c85f7eb2e8eed0
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 14:37:04 -0500

    feat(notification): add notification repository interface

--- src/main/java/com/primefuel/fulltank/platform/notification/domain/repositories/NotificationRepository.java
+++ src/main/java/com/primefuel/fulltank/platform/notification/domain/repositories/NotificationRepository.java
@@ -1,4 +1,13 @@
 package com.primefuel.fulltank.platform.notification.domain.repositories;
 
+import com.primefuel.fulltank.platform.notification.domain.model.aggregates.Notification;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface NotificationRepository {
+    Optional<Notification> findById(Long id);
+    List<Notification> findByUserId(Long userId);
+    List<Notification> findByUserIdAndReadFalse(Long userId);
+    Notification save(Notification notification);
 }
