c41fb0c21e837af549dedf187a73e408c7d66469
Diego Herrera <u202319027@upc.edu.pe>
2026-06-17 01:29:30 -0500

    feat(catalog): add infrastructure

---
+++ src/main/java/com/primefuel/fulltank/platform/catalog/domain/Infrastructure/persistence/jpa/entities/ProviderRatingPersistenceEntity.java
@@ -0,0 +1,28 @@
+package com.primefuel.fulltank.platform.catalog.domain.Infrastructure.persistence.jpa.entities;
+
+import com.primefuel.fulltank.platform.shared.infrastructure.persistence.jpa.entities.AuditableAbstractPersistenceEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "provider_ratings",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"company_id", "provider_id"}))
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProviderRatingPersistenceEntity extends AuditableAbstractPersistenceEntity {
+
+    @Column(nullable = false)
+    private Long companyId;
+
+    @Column(nullable = false)
+    private Long providerId;
+
+    @Column(nullable = false)
+    private Integer rating;
+}
