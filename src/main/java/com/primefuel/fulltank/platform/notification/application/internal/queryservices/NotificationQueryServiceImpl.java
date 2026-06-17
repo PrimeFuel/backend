369802325d2277cd75718718ba799aa512862164
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 14:41:58 -0500

    feat(notification): add query service for notifications

--- src/main/java/com/primefuel/fulltank/platform/notification/application/internal/queryservices/NotificationQueryServiceImpl.java
+++ src/main/java/com/primefuel/fulltank/platform/notification/application/internal/queryservices/NotificationQueryServiceImpl.java
@@ -1,4 +1,37 @@
 package com.primefuel.fulltank.platform.notification.application.internal.queryservices;
 
-public class NotificationQueryServiceImpl {
+import com.primefuel.fulltank.platform.notification.application.queryservices.NotificationQueryService;
+import com.primefuel.fulltank.platform.notification.domain.model.aggregates.Notification;
+import com.primefuel.fulltank.platform.notification.domain.model.queries.GetNotificationByIdQuery;
+import com.primefuel.fulltank.platform.notification.domain.model.queries.GetNotificationsByUserIdQuery;
+import com.primefuel.fulltank.platform.notification.domain.model.queries.GetUnreadNotificationsByUserIdQuery;
+import com.primefuel.fulltank.platform.notification.domain.repositories.NotificationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NotificationQueryServiceImpl implements NotificationQueryService {
+
+    private final NotificationRepository notificationRepository;
+
+    public NotificationQueryServiceImpl(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    @Override
+    public Optional<Notification> handle(GetNotificationByIdQuery query) {
+        return notificationRepository.findById(query.notificationId());
+    }
+
+    @Override
+    public List<Notification> handle(GetNotificationsByUserIdQuery query) {
+        return notificationRepository.findByUserId(query.userId());
+    }
+
+    @Override
+    public List<Notification> handle(GetUnreadNotificationsByUserIdQuery query) {
+        return notificationRepository.findByUserIdAndReadFalse(query.userId());
+    }
 }
--- src/main/java/com/primefuel/fulltank/platform/notification/application/queryservices/NotificationQueryService.java
+++ src/main/java/com/primefuel/fulltank/platform/notification/application/queryservices/NotificationQueryService.java
@@ -1,4 +1,15 @@
 package com.primefuel.fulltank.platform.notification.application.queryservices;
 
+import com.primefuel.fulltank.platform.notification.domain.model.aggregates.Notification;
+import com.primefuel.fulltank.platform.notification.domain.model.queries.GetNotificationByIdQuery;
+import com.primefuel.fulltank.platform.notification.domain.model.queries.GetNotificationsByUserIdQuery;
+import com.primefuel.fulltank.platform.notification.domain.model.queries.GetUnreadNotificationsByUserIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface NotificationQueryService {
+    Optional<Notification> handle(GetNotificationByIdQuery query);
+    List<Notification> handle(GetNotificationsByUserIdQuery query);
+    List<Notification> handle(GetUnreadNotificationsByUserIdQuery query);
 }
