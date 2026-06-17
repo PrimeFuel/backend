bcc66c7a56106d7f57a2b704d140dc237068e48e
Diego Herrera <u202319027@upc.edu.pe>
2026-06-17 01:29:30 -0500

    feat(catalog): add infrastructure

---
+++ src/main/java/com/primefuel/fulltank/platform/catalog/domain/Infrastructure/persistence/jpa/repositories/ProviderRatingPersistenceRepository.java
@@ -0,0 +1,16 @@
+package com.primefuel.fulltank.platform.catalog.domain.Infrastructure.persistence.jpa.repositories;
+
+import com.primefuel.fulltank.platform.catalog.infrastructure.persistence.jpa.entities.ProviderRatingPersistenceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProviderRatingPersistenceRepository
+        extends JpaRepository<ProviderRatingPersistenceEntity, Long> {
+    Optional<ProviderRatingPersistenceEntity> findByCompanyIdAndProviderId(Long companyId, Long providerId);
+    List<ProviderRatingPersistenceEntity> findByCompanyId(Long companyId);
+    List<ProviderRatingPersistenceEntity> findByProviderId(Long providerId);
+    List<ProviderRatingPersistenceEntity> findByCompanyIdAndProviderIdOrderById(
+            Long companyId, Long providerId);
+}
