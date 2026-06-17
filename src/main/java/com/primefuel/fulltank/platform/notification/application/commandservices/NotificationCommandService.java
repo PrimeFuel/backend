748af12ebf0ff1fc183cea83bf3fff2b77a47fe3
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 14:40:58 -0500

    feat(notification): add command service for notifications

--- src/main/java/com/primefuel/fulltank/platform/notification/application/commandservices/NotificationCommandService.java
+++ src/main/java/com/primefuel/fulltank/platform/notification/application/commandservices/NotificationCommandService.java
@@ -1,4 +1,12 @@
 package com.primefuel.fulltank.platform.notification.application.commandservices;
 
+import com.primefuel.fulltank.platform.notification.domain.model.aggregates.Notification;
+import com.primefuel.fulltank.platform.notification.domain.model.commands.CreateNotificationCommand;
+import com.primefuel.fulltank.platform.notification.domain.model.commands.MarkNotificationAsReadCommand;
+import com.primefuel.fulltank.platform.shared.application.result.ApplicationError;
+import com.primefuel.fulltank.platform.shared.application.result.Result;
+
 public interface NotificationCommandService {
+    Result<Notification, ApplicationError> handle(CreateNotificationCommand command);
+    Result<Notification, ApplicationError> handle(MarkNotificationAsReadCommand command);
 }
