5361a6196776d9cab76c9d245373b41be03c44a9
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 19:30:10 -0500

    feat(inventory): add query service for fuel products

--- src/main/java/com/primefuel/fulltank/platform/inventory/application/internal/queryservices/FuelProductQueryServiceImpl.java
+++ src/main/java/com/primefuel/fulltank/platform/inventory/application/internal/queryservices/FuelProductQueryServiceImpl.java
@@ -1,4 +1,37 @@
 package com.primefuel.fulltank.platform.inventory.application.internal.queryservices;
 
-public class FuelProductQueryServiceImpl {
+import com.primefuel.fulltank.platform.inventory.application.queryservices.FuelProductQueryService;
+import com.primefuel.fulltank.platform.inventory.domain.model.aggregates.FuelProduct;
+import com.primefuel.fulltank.platform.inventory.domain.model.queries.GetAllFuelProductsQuery;
+import com.primefuel.fulltank.platform.inventory.domain.model.queries.GetFuelProductByIdQuery;
+import com.primefuel.fulltank.platform.inventory.domain.model.queries.GetFuelProductsByProviderIdQuery;
+import com.primefuel.fulltank.platform.inventory.domain.repositories.FuelProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FuelProductQueryServiceImpl implements FuelProductQueryService {
+
+    private final FuelProductRepository fuelProductRepository;
+
+    public FuelProductQueryServiceImpl(FuelProductRepository fuelProductRepository) {
+        this.fuelProductRepository = fuelProductRepository;
+    }
+
+    @Override
+    public Optional<FuelProduct> handle(GetFuelProductByIdQuery query) {
+        return fuelProductRepository.findById(query.fuelProductId());
+    }
+
+    @Override
+    public List<FuelProduct> handle(GetAllFuelProductsQuery query) {
+        return fuelProductRepository.findAll();
+    }
+
+    @Override
+    public List<FuelProduct> handle(GetFuelProductsByProviderIdQuery query) {
+        return fuelProductRepository.findByProviderId(query.providerId());
+    }
 }
--- src/main/java/com/primefuel/fulltank/platform/inventory/application/queryservices/FuelProductQueryService.java
+++ src/main/java/com/primefuel/fulltank/platform/inventory/application/queryservices/FuelProductQueryService.java
@@ -1,4 +1,16 @@
 package com.primefuel.fulltank.platform.inventory.application.queryservices;
 
+import com.primefuel.fulltank.platform.inventory.domain.model.aggregates.FuelProduct;
+import com.primefuel.fulltank.platform.inventory.domain.model.queries.GetAllFuelProductsQuery;
+import com.primefuel.fulltank.platform.inventory.domain.model.queries.GetFuelProductByIdQuery;
+import com.primefuel.fulltank.platform.inventory.domain.model.queries.GetFuelProductsByProviderIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface FuelProductQueryService {
+    Optional<FuelProduct> handle(GetFuelProductByIdQuery query);
+    List<FuelProduct> handle(GetAllFuelProductsQuery query);
+    List<FuelProduct> handle(GetFuelProductsByProviderIdQuery query);
 }
+
