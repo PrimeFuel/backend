31cb9ae0ddc3b056f4516f7db4e91631bfef097d
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 19:27:51 -0500

    feat(inventory): add fuel product repository interface

--- src/main/java/com/primefuel/fulltank/platform/inventory/domain/repositories/FuelProductRepository.java
+++ src/main/java/com/primefuel/fulltank/platform/inventory/domain/repositories/FuelProductRepository.java
@@ -1,4 +1,15 @@
 package com.primefuel.fulltank.platform.inventory.domain.repositories;
 
+import com.primefuel.fulltank.platform.inventory.domain.model.aggregates.FuelProduct;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface FuelProductRepository {
+    Optional<FuelProduct> findById(Long id);
+    List<FuelProduct> findAll();
+    List<FuelProduct> findByProviderId(Long providerId);
+    FuelProduct save(FuelProduct fuelProduct);
+    boolean existsById(Long id);
+    void deleteById(Long id);
 }
