b67da02512401789085414b0f072fe5181d67e73
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 19:22:50 -0500

    feat(inventory): add create fuel product command

--- src/main/java/com/primefuel/fulltank/platform/inventory/domain/model/commands/CreateFuelProductCommand.java
+++ src/main/java/com/primefuel/fulltank/platform/inventory/domain/model/commands/CreateFuelProductCommand.java
@@ -1,4 +1,14 @@
 package com.primefuel.fulltank.platform.inventory.domain.model.commands;
 
-public record CreateFuelProductCommand() {
+import com.primefuel.fulltank.platform.inventory.domain.model.valueobjects.FuelType;
+
+public record CreateFuelProductCommand(
+        String name,
+        FuelType fuelType,
+        Double pricePerUnit,
+        String unit,
+        Double availableStock,
+        Double capacity,
+        Long providerId,
+        Boolean active) {
 }
