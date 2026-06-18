72e5a94291cc53e15410734b3c832d8f6e91f282
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 19:38:33 -0500

    feat(inventory): add transformers between resources and entities

--- src/main/java/com/primefuel/fulltank/platform/inventory/interfaces/rest/transform/CreateFuelProductCommandFromResourceAssembler.java
+++ src/main/java/com/primefuel/fulltank/platform/inventory/interfaces/rest/transform/CreateFuelProductCommandFromResourceAssembler.java
@@ -1,4 +1,16 @@
 package com.primefuel.fulltank.platform.inventory.interfaces.rest.transform;
 
-public class CreateFuelProductCommandFromResourceAssembler {
+import com.primefuel.fulltank.platform.inventory.domain.model.commands.CreateFuelProductCommand;
+import com.primefuel.fulltank.platform.inventory.interfaces.rest.resources.CreateFuelProductResource;
+
+public final class CreateFuelProductCommandFromResourceAssembler {
+
+    private CreateFuelProductCommandFromResourceAssembler() {
+    }
+
+    public static CreateFuelProductCommand toCommandFromResource(CreateFuelProductResource resource) {
+        return new CreateFuelProductCommand(resource.name(), resource.fuelType(), resource.pricePerUnit(),
+                resource.unit(), resource.availableStock(), resource.capacity(), resource.providerId(),
+                resource.active());
+    }
 }
