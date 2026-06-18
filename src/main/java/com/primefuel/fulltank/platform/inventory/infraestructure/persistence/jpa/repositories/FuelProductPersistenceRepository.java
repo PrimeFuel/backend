272b84133ff72c9bf00bbea7536bce69d489b88f
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 19:32:17 -0500

    feat(inventory): add JPA repository for fuel product persistence

--- src/main/java/com/primefuel/fulltank/platform/inventory/infraestructure/persistence/jpa/repositories/FuelProductPersistenceRepository.java
+++ src/main/java/com/primefuel/fulltank/platform/inventory/infraestructure/persistence/jpa/repositories/FuelProductPersistenceRepository.java
@@ -1,4 +1,12 @@
-package com.primefuel.fulltank.platform.inventory.infraestructure.persistence.jpa.repositories;
+package com.primefuel.fulltank.platform.inventory.infrastructure.persistence.jpa.repositories;
 
-public class FuelProductPersistenceRepository {
+import com.primefuel.fulltank.platform.inventory.infrastructure.persistence.jpa.entities.FuelProductPersistenceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FuelProductPersistenceRepository extends JpaRepository<FuelProductPersistenceEntity, Long> {
+    List<FuelProductPersistenceEntity> findByProviderId(Long providerId);
 }
