b56e350b74f3462073913f49ea5d5fc0bb1d10fb
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 19:39:39 -0500

    feat(inventory): add transformers for update commands

--- src/main/java/com/primefuel/fulltank/platform/inventory/interfaces/rest/transform/UpdateFuelProductStockCommandFromResourceAssembler.java
+++ src/main/java/com/primefuel/fulltank/platform/inventory/interfaces/rest/transform/UpdateFuelProductStockCommandFromResourceAssembler.java
@@ -1,4 +1,15 @@
 package com.primefuel.fulltank.platform.inventory.interfaces.rest.transform;
 
-public class UpdateFuelProductStockCommandFromResourceAssembler {
+import com.primefuel.fulltank.platform.inventory.domain.model.commands.UpdateFuelProductStockCommand;
+import com.primefuel.fulltank.platform.inventory.interfaces.rest.resources.UpdateFuelProductStockResource;
+
+public final class UpdateFuelProductStockCommandFromResourceAssembler {
+
+    private UpdateFuelProductStockCommandFromResourceAssembler() {
+    }
+
+    public static UpdateFuelProductStockCommand toCommandFromResource(Long fuelProductId,
+                                                                      UpdateFuelProductStockResource resource) {
+        return new UpdateFuelProductStockCommand(fuelProductId, resource.newStock());
+    }
 }
