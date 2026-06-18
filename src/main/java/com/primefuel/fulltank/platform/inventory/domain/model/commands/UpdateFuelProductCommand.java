7d3a956c501108edc99399f2ec2a7f5adfba3bd0
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 19:23:29 -0500

    feat(inventory): add update fuel product command

--- src/main/java/com/primefuel/fulltank/platform/inventory/domain/model/commands/UpdateFuelProductCommand.java
+++ src/main/java/com/primefuel/fulltank/platform/inventory/domain/model/commands/UpdateFuelProductCommand.java
@@ -1,4 +1,7 @@
 package com.primefuel.fulltank.platform.inventory.domain.model.commands;
 
-public record UpdateFuelProductCommand() {
+import com.primefuel.fulltank.platform.inventory.domain.model.valueobjects.FuelType;
+
+public record UpdateFuelProductCommand(Long fuelProductId, String name, FuelType fuelType, Double pricePerUnit, String unit, Double availableStock, Double capacity, Boolean active) {
 }
+
