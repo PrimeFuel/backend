abfa33960c3887c517bac7aee2ed8f733b46aa8e
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2026-06-17 19:37:21 -0500

    feat(inventory): add REST resources for fuel product update operations

--- src/main/java/com/primefuel/fulltank/platform/inventory/interfaces/rest/resources/UpdateFuelProductStockResource.java
+++ src/main/java/com/primefuel/fulltank/platform/inventory/interfaces/rest/resources/UpdateFuelProductStockResource.java
@@ -1,4 +1,5 @@
 package com.primefuel.fulltank.platform.inventory.interfaces.rest.resources;
 
-public record UpdateFuelProductStockResource() {
+public record UpdateFuelProductStockResource(Double newStock) {
 }
+
